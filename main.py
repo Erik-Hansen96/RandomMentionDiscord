c8e50365b91fd04474886ad415c02c338e26f1a4
erik <ehansen1671@gmail.com>
2024-10-27 06:21:11 +0000

    comments n req

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 from discord.ext import commands
 from dotenv import load_dotenv
+
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
@@ -12,37 +13,49 @@ intents.message_content = True
 intents.messages = True
 
 bot = commands.Bot(command_prefix='@', intents=intents)
-user_cache = {}
 
 @bot.event
 async def on_ready():
 	print('logged in')
 
+# To avoid excessive API calls and increase speed
+server_cache = {}
+
+# Mention random user command
 @bot.command()
 async def someone(ctx):
 	server_id = ctx.guild.id
-	if server_id not in user_cache:
+
+	# Check if the server is already cached
+	if server_id not in server_cache:
 		user_mentions = []
+
+		# If not in cache, add all users to a list
 		for user in ctx.guild.members:
 			if not user.bot:
 				user_mentions.append(user.mention)
-		user_cache[server_id] = user_mentions
-	rando = random.choice(user_cache[server_id])
+
+		server_cache[server_id] = user_mentions # Add list to cache
+
+	rando = random.choice(server_cache[server_id])
 	await ctx.send(rando)
 
+# When a user joins a cached server, add them to the cache
 @bot.event
-async def on_member_join(member):
-	if member.guild.id in user_cache:
-		user_cache[member.guild.id].append(member.mention)
+async def on_member_join(user):
+	if user.guild.id in server_cache:
+		server_cache[user.guild.id].append(user.mention)
 
+# When a user leaves a cached server, remove them from cache
 @bot.event
-async def on_member_remove(member):
-	if member.guild.id in user_cache:
-		user_cache[member.guild.id].remove(member.mention)
+async def on_member_remove(user):
+	if user.guild.id in server_cache:
+		server_cache[user.guild.id].remove(user.mention)
 
+# If a server removes the bot, remove server from cache
 @bot.event
 async def on_guild_remove(guild):
-	if guild.id in user_cache:
-		del user_cache[guild.id]
+	if guild.id in server_cache:
+		del server_cache[guild.id]
 
 bot.run(TOKEN)
